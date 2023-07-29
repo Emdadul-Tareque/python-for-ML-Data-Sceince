f4aea1e1756de8e086b68b6d7b7569cf966823bf
emdadul-tareque <emdadultareque@gmail.com>
2023-07-29 13:39:28 +0600

    delete a folder

--- Python-Connecting with Databases and APIs/Working with python & sql/create-table-template.sql
+++
@@ -1,8 +0,0 @@
--- Active: 1690611250646@@127.0.0.1@3306@test
-CREATE TABLE if NOT exists  test1.test_table (c1 INT, c2 VARCHAR(50), c3 FLOAT, c4 INT, c5 VARCHAR(50));
-
-CREATE TABLE if NOT exists  test2.test_table (c1 INT, c2 VARCHAR(50), c3 FLOAT, c4 INT, c5 VARCHAR(50));
-
-insert into test1.test_table values(123, 'Emdadul', 32.3,  456, 'Tareque');
-
-SELECT * FROM test1.test_table
